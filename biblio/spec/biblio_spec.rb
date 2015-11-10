cc02a3346b207213ff1545021f64d1cbbcfb227a
alu0100769579 <alu0100769579@ull.edu.es>
2015-11-10 19:11:29 +0000

    Creada la estructura de trabajo de la práctica anterior

---
+++ biblio/spec/biblio_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Biblio do
+  it 'has a version number' do
+    expect(Biblio::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
